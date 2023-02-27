e81d9b32d761527a176b10f29183c3d7aab5367d
umin <108667311+mynameisumin@users.noreply.github.com>
2023-02-27 19:46:34 +0900

    Add files via upload

---
+++ 입출력과 사칙연산/div/spec.cpp
@@ -0,0 +1,46 @@
+
+#include <tcframe/spec.hpp>
+using namespace tcframe;
+
+class ProblemSpec : public BaseProblemSpec {
+protected:
+    int A, B;
+    double num;
+
+    void InputFormat() {
+        LINE(A, B);
+    }
+
+    void OutputFormat() {
+        LINE(num);
+    }
+
+    void GradingConfig() {
+        TimeLimit(1);
+        MemoryLimit(256);
+    }
+
+    void Constraints() {
+        CONS(0 <= A);
+        CONS(0 <= B && B <= 50);
+    }
+};
+
+class TestSpec : public BaseTestSpec<ProblemSpec> {
+protected:
+    void SampleTestCase1() {
+        Input({
+            "1 3"
+        });
+        Output({
+            "0.3333333333333333333333333333"
+        });
+    }
+
+    void TestCases() {
+        CASE(A = 55, B = 5);
+        CASE(A = 99, B = 10);
+        CASE(A = 124, B = 31);
+        CASE(A = rnd.nextInt(0, 1000), B = rnd.nextInt(0, 50));
+    }
+};
